bcb66738dcc7e4d14fc57641a37226ee9386d4c5
gamesuwatponchana <64387765+gamesuwatponchana@users.noreply.github.com>
2020-08-07 16:15:12 +0700

    commit@b1#0013

--- resources/views/ppr3welcome.blade.php
+++ resources/views/ppr3welcome.blade.php
@@ -11,44 +11,94 @@
         <link rel="icon" href="{{ asset('image_icon/icons/newppr3_logo_a8D_icon.ico') }}" type="image/x-icon">
         {{-- <link href="{{ asset('/css/theme/welcome.css') }}" rel="stylesheet" type="text/css" /> --}}
         <style>
-            html,
-            body {
-            min-height: 100%;
-            height: 100%;
-            background-image: url('{{ asset("image_icon/images/theme/king-bg.png") }}');
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: contain;
-            background-color: #b38b27;
-                /* background-color: #b380b8; */
-            }
-            .bg {
-                /* The image used */
-                background-image: url('{{ asset("image_icon/images/theme/king-cover.png") }}');
-                /* background-image: url('../../image/theam/queen/queencover001.png'); */
-                height: 100%;
+            @media only screen and (max-width: 720px) {
+                    html,
+                body {
+                width: 100%;
+                height: auto;
+                background-image: url('{{ asset("image_icon/images/theme/Q_/q_bg_002.png") }}');
                 background-position: center;
                 background-repeat: no-repeat;
                 background-size: contain;
-                margin-top: -30px;
-                /* Center and scale the image nicely */
-            }
+                /* background-color: #b38b27; */
+                    /* background-color: #b380b8; */
+                }
+                .bg {
+                    /* The image used */
+                    background-image: url('{{ asset("image_icon/images/theme/Q_/q-coverpage001.png") }}');
+                    /* background-image: url('../../image/theam/queen/queencover001.png'); */
+                    width: 100%;
+                    height: auto;
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                    /* margin-top: -30px; */
+                    /* Center and scale the image nicely */
+                }
 
-            .enter{
-                position: absolute;
-                margin-left: auto;
-                margin-right: auto;
-                left: 50%;
-                margin-left: -100px;
-                margin-top: -55px;
-                height: 50px;
+                .enter{
+                    /* position: absolute; */
+                    /* margin-left: auto; */
+                    /* margin-right: auto; */
+                    /* left: 50%; */
+                    /* margin-left: -100px; */
+                    /* margin-top: -55px; */
+                    /* height: 50px; */
+                    text-align: center;
+
+                }
             }
+            /* ******************************************************************************************** */
+            @media only screen and (min-width: 720px) {
+                    html,
+                body {
+                width: 100%;
+                height: 100%;
+                background-image: url('{{ asset("image_icon/images/theme/Q_/q_bg_001.png") }}');
+                background-position: center;
+                background-repeat: no-repeat;
+                /* background-size: 100% 100%; */
+                /* background-color: #b38b27; */
+                    /* background-color: #b380b8; */
+                }
+                .bg {
+                    /* The image used */
+                    background-image: url('{{ asset("image_icon/images/theme/Q_/q-coverpage001.png") }}');
+                    /* background-image: url('../../image/theam/queen/queencover001.png'); */
+                    height: 82%;
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                    /* margin-top: -30px; */
+                    /* Center and scale the image nicely */
+                }
+
+                .enter{
+                    /* position: absolute; */
+                    /* margin-left: auto; */
+                    /* margin-right: auto; */
+                    /* left: 50%; */
+                    /* margin-left: -100px; */
+                    /* margin-top: -55px; */
+                    /* height: 50px; */
+                    text-align: center;
+                    margin-bottom: 0px;
+                }
+                .img_enter{
+
+                }
+            }
+            /* ******************************************************************************************** */
+
         </style>
 
 	</head>
 	<body>
         <div class="bg"></div>
-        <br>
-        <a href=""><img class="enter"  src="{{ asset('image_icon/images/theme/enter-site.png') }}"></a>
+        {{-- <br> --}}
+        <div class="enter">
+            <a href=""><img class="img_enter" src="{{ asset('image_icon/images/theme/Q_/q_enter_site.png') }}"></a>
+        </div>
+
 	</body>
 </html>
